2aab4db6f2cfe545da46f3502ece5d0e054229cd
Badr Al-aseeri <badralaseeri@u.boisestate.esu>
2017-03-11 03:07:11 -0700

    fixed login issue

--- AnonU/app/src/main/java/appathon/anonu/LoginActivity.java
+++ AnonU/app/src/main/java/appathon/anonu/LoginActivity.java
@@ -1,14 +1,31 @@
 package appathon.anonu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static RequestQueue queue = null;
+    private static String confirmationToken;
+    private static boolean emailConfirmation = false;
+    private static User user;
 
     private TextView email;
     private EditText addEmail;
@@ -33,11 +50,54 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    public void verifyEmail(Context context, String email){
+        if( queue == null){
+            queue = Volley.newRequestQueue(context);
+        }
+        String url ="https://anonymousuniversity.com/api/set/users.php?email=" + email;
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("BA", response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            emailConfirmation = jsonObject.getBoolean("successful");
+                            String parsedID = jsonObject.getString("id");
+                            confirmationToken = jsonObject.getString("token");
+                            int parsedScore = jsonObject.getInt("score");
+                            user = new User(parsedID, parsedScore, confirmationToken);
+                            if(emailConfirmation == true) {
+                                Log.d("BA", "inside check is true");
+                                Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(homeIntent);
+                            }
+                            Log.d("BA", "HERE");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("BA", "That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
+        queue.start();
+    }
+
     private void loginClick() {
-        WebServicesController.verifyEmail(this, addEmail.getText().toString());
-        if(WebServicesController.isEmailConfirmation() == true) {
+        verifyEmail(this, addEmail.getText().toString());
+
+     /*   Log.d("BA", "outside of check");
+
+        if(emailConfirmation == true) {
+            Log.d("BA", "inside check is true");
             Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(homeIntent);
-        }
+        } */
     }
 }
--- AnonU/app/src/main/java/appathon/anonu/WebServicesController.java
+++ AnonU/app/src/main/java/appathon/anonu/WebServicesController.java
@@ -24,9 +24,10 @@ public class WebServicesController {
 
     private static RequestQueue queue = null;
     private static String confirmationToken;
-    private static boolean emailConfirmation;
+    private static boolean emailConfirmation = false;
+    private static String parsedPostID;
     private static User user;
-
+/*
     public static void verifyEmail(Context context, String email){
         if( queue == null){
             queue = Volley.newRequestQueue(context);
@@ -40,16 +41,12 @@ public class WebServicesController {
                         // Display the first 500 characters of the response string.
                         Log.d("BA", response);
                         try {
-                            JSONArray jsonArray = new JSONArray(response);
-                            for(int index = 0; index < jsonArray.length(); index++) {
-                                JSONObject jsonObject = jsonArray.getJSONObject(index);
-                                JSONObject successfulJSONObject = jsonObject.getJSONObject("successful");
-                                emailConfirmation = successfulJSONObject.getBoolean("successful");
-                                String parsedID = successfulJSONObject.getString("id");
-                                confirmationToken = successfulJSONObject.getString("token");
-                                int parsedScore = successfulJSONObject.getInt("score");
+                            JSONObject jsonObject = new JSONObject(response);
+                                emailConfirmation = jsonObject.getBoolean("successful");
+                                String parsedID = jsonObject.getString("id");
+                                confirmationToken = jsonObject.getString("token");
+                                int parsedScore = jsonObject.getInt("score");
                                 user = new User(parsedID, parsedScore, confirmationToken);
-                            }
                             Log.d("BA", "HERE");
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -64,7 +61,7 @@ public class WebServicesController {
         queue.add(stringRequest);
         queue.start();
     }
-
+*/
     public User getUser(){
         return user;
     }
@@ -92,14 +89,14 @@ public class WebServicesController {
                                 JSONObject jsonObject = jsonArray.getJSONObject(index);
                                 JSONObject successfulJSONObject = jsonObject.getJSONObject("successful");
                                 boolean successPosts = successfulJSONObject.getBoolean("successful");
-                                String parsedID = successfulJSONObject.getString("id");
+                                parsedPostID = successfulJSONObject.getString("id");
                                 String parsedOwnerID = successfulJSONObject.getString("owner_id");
                                 String parsedSchoolID = successfulJSONObject.getString("school_id");
                                 String parsedDateUtc = successfulJSONObject.getString("date_utc");
                                 String parsedContent = successfulJSONObject.getString("contents");
                                 int parsedCommentCount = successfulJSONObject.getInt("comment_count");
                                 int parsedVoteCount = successfulJSONObject.getInt("vote_count");
-                                Post post = new Post(parsedID, parsedOwnerID, parsedSchoolID, parsedDateUtc, parsedContent, parsedCommentCount, parsedVoteCount);
+                                Post post = new Post(parsedPostID, parsedOwnerID, parsedSchoolID, parsedDateUtc, parsedContent, parsedCommentCount, parsedVoteCount);
                                 posts.add(post);
                             }
                             PostsController.addPosts(posts);
@@ -121,7 +118,7 @@ public class WebServicesController {
         if( queue == null){
             queue = Volley.newRequestQueue(context);
         }
-        String url ="https://anonymousuniversity.com/api/set/users.php?email=" + emailConfirmation;
+        String url ="https://anonymousuniversity.com/api/get/comments.php?token=" + emailConfirmation + "&post_id=" + parsedPostID;
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -134,11 +131,14 @@ public class WebServicesController {
                             for(int index = 0; index < jsonArray.length(); index++) {
                                 JSONObject jsonObject = jsonArray.getJSONObject(index);
                                 JSONObject successfulJSONObject = jsonObject.getJSONObject("successful");
-                                emailConfirmation = successfulJSONObject.getBoolean("successful");
-                                String parsedID = successfulJSONObject.getString("id");
-                                confirmationToken = successfulJSONObject.getString("token");
-                                int parsedScore = successfulJSONObject.getInt("score");
-                                user = new User(parsedID, parsedScore, confirmationToken);
+                                boolean successPosts = successfulJSONObject.getBoolean("successful");
+                                parsedPostID = successfulJSONObject.getString("id");
+                                String parsedOwnerID = successfulJSONObject.getString("owner_id");
+                                String parsedSchoolID = successfulJSONObject.getString("school_id");
+                                String parsedDateUtc = successfulJSONObject.getString("date_utc");
+                                String parsedContent = successfulJSONObject.getString("contents");
+                                int parsedCommentCount = successfulJSONObject.getInt("comment_count");
+                                int parsedVoteCount = successfulJSONObject.getInt("vote_count");
                             }
                             Log.d("BA", "HERE");
                         } catch (JSONException e) {
